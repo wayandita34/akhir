fd0673e37ebb0b08c06bbd050e424758dd1a20be
wayandita34 <wayandita34@gmail.com>
2023-06-22 19:44:15 +0700

    master : upload Dashboard.kt

--- app/src/main/java/com/example/aplikasi/Akun.kt
+++ app/src/main/java/com/example/aplikasi/Akun.kt
@@ -2,8 +2,9 @@ package com.example.aplikasi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 
-class Akun : AppCompatActivity() {
+class Akun : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.akun)
