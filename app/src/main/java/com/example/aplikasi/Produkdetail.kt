dbe8087a093c4eef749f432ad0538d29d27c45a3
Alvin233Rama <alvinramadhan20311027@gmail.com>
2023-06-22 21:26:23 +0700

    alvin:Upload Produkdetail.kt

---
+++ app/src/main/java/com/example/aplikasi/Produkdetail.kt
@@ -0,0 +1,12 @@
+package com.example.aplikasikita
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class Produkdetail : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.produkdetail)
+    }
+}
