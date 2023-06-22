c0d177d20eb0ef99e9f71fb4c03cf5137f56744b
wayandita34 <wayandita34@gmail.com>
2023-06-22 19:56:40 +0700

    master : upload Kategori.kt

---
+++ app/src/main/java/com/example/aplikasi/Kategori.kt
@@ -0,0 +1,26 @@
+package com.example.aplikasi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.activity.ComponentActivity
+
+class Kategori : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.kategori);
+
+//        interface list kategori
+        val lv_kategori:ListView = findViewById(R.id.lv_kategori);
+
+//        membuat data kategori
+        val nama_kategoris:List<String> = ListOf("Jacket","Shirt","T-shirt","Pants","Aksesoris");
+
+//        membuat adapter yang berisikan perulangan data
+        val perulangan_data:ArrayAdapter<String> = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,nama_kategoris);
+
+//        pasang perulangan data di interface lv_kategori
+        lv_kategori.adapter = perulangan_data;
+    }
+}
