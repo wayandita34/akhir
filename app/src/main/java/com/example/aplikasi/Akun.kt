44a98d96423372e6238153a204230bab7662440d
wayandita34 <wayandita34@gmail.com>
2023-06-22 19:19:35 +0700

    master : upload Akun.kt

---
+++ app/src/main/java/com/example/aplikasi/Akun.kt
@@ -0,0 +1,11 @@
+package com.example.aplikasi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Akun : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.akun)
+    }
+}
